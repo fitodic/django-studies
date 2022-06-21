6f62cfe7c9a02a383fbcee20caa4c2a5b5f93198
Filip Todic <todic.filip@gmail.com>
2022-06-21 18:01:46 +0200

    Add the package boilerplate

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+setup(
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+)
