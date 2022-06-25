2195a11f600642d41e91bcf09efbb363e75c349b
Filip Todic <todic.filip@gmail.com>
2022-06-25 08:59:34 +0200

    Update settings

--- demo/demo/asgi.py
+++ demo/demo/asgi.py
@@ -1,17 +1,9 @@
-"""
-ASGI config for demo project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
 import os
 
-from django.core.asgi import get_asgi_application
+from configurations.asgi import get_asgi_application
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Demo")
 
 application = get_asgi_application()
--- demo/demo/wsgi.py
+++ demo/demo/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for demo project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Demo")
 
 application = get_wsgi_application()
